105ccef51503dca0366a6e69ef9bef34b2196fdd
ag739 <ag739@cornell.edu>
2017-09-25 16:43:39 -0400

    updated settings

--- on-this-day/settings.py
+++ on-this-day/settings.py
@@ -82,9 +82,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'assets')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
